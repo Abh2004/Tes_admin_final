97c1ffa5bd17cd122f83bd0cb490026ad35ee168
Abh2004 <asm2004@kgpian.iitkgp.ac.in>
2023-08-20 17:30:48 +0530

    nav and team section

---
+++ tes/tesapp/urls.py
@@ -0,0 +1,51 @@
+from django.urls import path
+
+from .views import (
+    PhotoListView,
+    PhotoTagListView,
+    PhotoDetailView,
+    PhotoCreateView,
+    PhotoUpdateView,
+    PhotoDeleteView,
+    homeview,
+    FileListView,
+    subscribe_view,
+    
+)
+from tesapp import views
+
+
+app_name = 'photo'
+
+urlpatterns = [
+    path('', homeview, name='home'),
+    path('photo/list/', PhotoListView.as_view(), name='list'),
+    path('photo/articles/', FileListView.as_view(), name='article'),
+
+    path('tag/<slug:tag>/', PhotoTagListView.as_view(), name='tag'),
+
+    path('photo/<int:pk>/', PhotoDetailView.as_view(), name='detail'),
+
+    path('photo/create/', PhotoCreateView.as_view(), name='create'),
+
+    path('photo/<int:pk>/update/', PhotoUpdateView.as_view(), name='update'),
+
+    path('photo/<int:pk>/delete/', PhotoDeleteView.as_view(), name='delete'),
+    # path('success/', views.success_view, name='success_page'),
+    # path('subscribe/', views.subscribe_view, name='subscribe_view'),
+
+    # path('subscribe_page/', views.subscribe_page , name='subscribe'),
+    
+
+
+
+    path('contact/', views.contact_view , name='contact'),
+    path('subscribe/', views.subscribe_view , name='subscribe'),
+
+
+
+
+
+
+
+]
