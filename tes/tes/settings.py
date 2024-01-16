fb6c229b120d8ac760a864a4fcc7012e8406faec
Maurya <abhisheksinghmaurya2004@gmail.com>
2024-01-16 23:09:19 +0530

    something

--- tes/tes/settings.py
+++ tes/tes/settings.py
@@ -72,19 +72,19 @@ WSGI_APPLICATION = 'tes.wsgi.application'
 #         'HOST': 'localhost',
 #         'PORT': '5432',
 #     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
+# # }
+#  DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#      }
 # }
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / "db.sqlite3",
     },
 }
 
