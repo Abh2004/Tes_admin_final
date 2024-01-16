acf8d9939b52ba4d6e8574a36c3860e2b69374b0
Maurya <abhisheksinghmaurya2004@gmail.com>
2024-01-16 22:40:25 +0530

    changed build versionn

--- tes/build_file.sh
+++ tes/build_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y libsqlite3-dev
+apt-get install -y libsqlite3-dev  # Install development files for SQLite
 
 pip3 install --disable-pip-version-check --target . --upgrade -r /vercel/path0/tes/requirements.txt
 python3.9 manage.py collectstatic --noinput
